ff168a71ef4812d11fe1b52ffcb381b7e209ca38
c00ldude1oo <crluke11@gmail.com>
2018-03-06 00:24:14 -0500

    Initial Commit

---
+++ me.c00ldude1oo.connectionlogger/src/me/c00ldude1oo/connectionlogger/Main.java
@@ -0,0 +1,12 @@
+package me.c00ldude1oo.connectionlogger;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Main extends Plugin {
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("COnnectionLogger has been enabled");
+	}
+	
+}
